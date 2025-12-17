30608f734dfb37550a60450063534cf2eacbdbc9
evineric <eric_evin@hotmail.com>
2025-12-17 12:04:34 +0100

    correction App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,20 +3,10 @@ import FlexH3Grow from "../components/layout/FlexH3Grow/FlexH3Grow";
 import Header from "../components/ui/Header/Header";
 import Footer from "../components/ui/Footer/Footer";
 import Navbar from "../components/ui/Navbar/Navbar";
-import MemeForm from "../components/functionnal/MemeForm/MemeForm";
-import { emptyMeme, type MemeInterface, MemeSVGViewer } from "orsys-tjs-meme";
-import { useEffect, useState } from "react";
-import { REST_API_RESSOURCES, REST_API_URL } from "../config/constantes";
-import { type ImageInterface } from "orsys-tjs-meme";
-type TImages=Array<ImageInterface>;
+import MemeForm from "../components/functionnal/MemeForm/MemeForm.store";
+import MemeSvgViewer from "../components/ui/MemeSvgViewer/MemeSvgViewer.store";
 
 const App: React.FC<undefined> = () => {
-  const [images, setImages] = useState<TImages>([]);
-  const [current, setCurrent] = useState<MemeInterface>(emptyMeme);
-
-  useEffect(() => {
-    fetch(`${REST_API_URL}${REST_API_RESSOURCES.images}`).then(r=>r.json()).then(imgs=>setImages(imgs));
-  },[])
   return (
     <>
     <div className="App">
@@ -24,8 +14,8 @@ const App: React.FC<undefined> = () => {
         <Header/>
         <Navbar/>
         <FlexV1Grow>
-          <MemeSVGViewer meme={current} image={images.find(i=>i.id === current.imageId)} basePath="" />
-          <MemeForm meme={current} images={images} onMemeChange={setCurrent}/>
+          <MemeSvgViewer basePath="" />
+          <MemeForm/>
         </FlexV1Grow>
         <Footer/>
       </FlexH3Grow>
