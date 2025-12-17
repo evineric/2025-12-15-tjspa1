65b62756f666f5497d3ace04dca4372b6f5f8f90
evineric <eric_evin@hotmail.com>
2025-12-17 10:29:34 +0100

    création des slices et du store

---
+++ src/store/ressources.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
+import {images,memes} from "../../db.json";
+
+interface IRessourcesState{
+    memes:Array<MemeInterface>;
+    images:Array<ImageInterface>;
+    loaded:boolean;
+}
+
+const initialState = {
+ memes:memes,
+ images:images,
+ loaded:true
+}
+
+const ressources = createSlice({
+  name: 'ressources',
+  initialState,
+  reducers: {
+
+  }
+});
+
+//export const {} = ressources.actions
+
+const ressourcesReducer= ressources.reducer;
+export default ressourcesReducer;
---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import ressourcesReducer from "./ressources";
+import currentReducer from "./current";
+
+const store = configureStore({
+    reducer: {ressources: ressourcesReducer, current:currentReducer},
+});
+store.subscribe(()=>{console.log(store.getState())});
